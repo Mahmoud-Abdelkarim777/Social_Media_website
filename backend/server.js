11ac4c2cfdd5bbc9b75769bf32d0005823b6fdad
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2026-05-14 23:35:52 +0300

    fix railway port binding

--- backend/server.js
+++ backend/server.js
@@ -53,17 +53,17 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => {
   console.log("MongoDB Connected 🔥");
 
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});;
+  app.listen(process.env.PORT, "0.0.0.0", () => {
+    console.log("Server running");
+  });
 })
 .catch((err) => {
   console.log("MongoDB Error:", err.message);
 
   // تشغيل السيرفر حتى لو MongoDB فشل
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(process.env.PORT, "0.0.0.0", () => {
+    console.log("Server running");
+  });
 });
 // =========================
 // Auth Middleware
