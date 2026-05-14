16f036bae128136771bf4f33b6452f388ca92ca7
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2026-05-14 11:10:13 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const path = require("path");
 const Post = require("./models/Post");
 const User = require("./models/User");
 
+const PORT = process.env.PORT || 5000;
+
+
 const app = express();
 // =========================
 // Middleware (الترتيب هنا هو السر)
@@ -41,13 +44,6 @@ const upload = multer({ storage });
 // =========================
 // Middleware
 // =========================
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb', extended: true }));
-// app.use(cors({
-//       origin: "*",
-//       methods: ["GET", "POST", "PUT", "DELETE"],
-//       allowedHeaders: ["Content-Type", "Authorization"]
-//     }));
 
 // =========================
 // MongoDB Connection
@@ -59,8 +55,8 @@ mongoose
   // =========================
   // Server Start
   // =========================
-    app.listen(5000, () => {
-      console.log("Server running on http://localhost:5000 🚀");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
 
   })
